e1da527db3c8ade75003e92674d23b85771ac1b1
ellen <ellen.vitoria.ogs@gmail.com>
2021-09-29 18:55:13 -0300

    Utilizando Design Pattern Facade na classe Combo

---
+++ Combo.java
@@ -0,0 +1,24 @@
+public class Combo {
+    private Item item;
+    private PratoPrincipal prato;
+
+    //Método Construtor
+    public Combo(int cmb) {
+        System.out.println("Combo " + cmb);
+        prato = new PratoPrincipal();
+    }
+
+    //Acessando outras classes para criar Combo
+    public void criarCombo(int cd, String desc, double preco) {
+        item = new Item();
+        item.setCodigo(cd);
+        item.setDescricao(desc);
+        item.setPreco(preco);
+        prato.adicionar(item); //prato.itens.add(item);
+    }
+
+    //Mostrando items do Combo através de PratoPrincipal
+    public void mostrarCombo() {
+        prato.mostrarItens();
+    }
+}
